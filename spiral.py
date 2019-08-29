598cddc8ef3e8554da74db58589ff1316bf2862f
Ike <ikeholzmann@gmail.com>
2019-08-29 15:45:08 -0700

    Alternating colors

--- spiral.py
+++ spiral.py
@@ -5,18 +5,32 @@ Mohanad = turtle.Turtle()
 
 Mohanad.speed(10)
 
-Mohanad.color("DarkBlack")
+colors = [
+  "Red",
+  "Orange",
+  "Yellow",
+  "Green",
+  "Blue",
+  "Purple"
+]
+
+colorIndex = -1
 
 for i in range(100):
-    Mohanad.forward(180)
-    Mohanad.left(70)
-    Mohanad.forward(60)
-    Mohanad.right(40)
+    colorIndex += 1
+    if colorIndex == 6:
+      colorIndex=-1
+    else:
+      Mohanad.color(colors[colorIndex])
+      Mohanad.forward(180)
+      Mohanad.left(70)
+      Mohanad.forward(60)
+      Mohanad.right(40)
 
-    Mohanad.penup()
-    Mohanad.setposition(0, 0)
-    Mohanad.pendown()
+      Mohanad.penup()
+      Mohanad.setposition(0, 0)
+      Mohanad.pendown()
 
-    Mohanad.right(2)
+      Mohanad.right(2)
 
 turtle.done()
